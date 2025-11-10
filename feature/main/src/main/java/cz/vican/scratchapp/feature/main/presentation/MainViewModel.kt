e866b6fcbc876e571a356c91d3cb7385fff00715
Peter Vican <vican.peter@gmail.com>
2025-11-10 23:43:15 +0100

    update main stuff

--- feature/main/src/main/java/cz/vican/scratchapp/feature/main/presentation/MainViewModel.kt
+++ feature/main/src/main/java/cz/vican/scratchapp/feature/main/presentation/MainViewModel.kt
@@ -1,19 +1,32 @@
 package cz.vican.scratchapp.feature.main.presentation
 
+import androidx.lifecycle.viewModelScope
 import cz.vican.scratchapp.feature.main.domain.MainNavigationController
 import cz.vican.scratchapp.feature.main.presentation.MainViewModel.UiState
 import cz.vican.scratchapp.library.architecture.presentation.AbstractViewModel
+import cz.vican.scratchcard.generic.scratchcard.domain.ObserveScratchStatusUseCase
+import kotlinx.coroutines.launch
 
 
 internal class MainViewModel(
-    private val navigationController: MainNavigationController
+    private val navigationController: MainNavigationController,
+    private val observeScratchStatusUseCase: ObserveScratchStatusUseCase
 ) : AbstractViewModel<UiState>() {
+
+    init {
+        viewModelScope.launch {
+            observeScratchStatusUseCase.invoke().collect {
+                setState { copy(scratchState = it.status.name) }
+            }
+        }
+    }
 
     override fun createInitialState(): UiState {
         return UiState(
             title = "Main Screen",
             scratchButtonText = "Scratch Card",
-            activateButtonText = "Activate Card"
+            activateButtonText = "Activate Card",
+            scratchState = ""
         )
     }
 
@@ -28,6 +41,7 @@ internal class MainViewModel(
 
     data class UiState(
         val title: String,
+        val scratchState: String,
         val scratchButtonText: String,
         val activateButtonText: String
     ) : IState
