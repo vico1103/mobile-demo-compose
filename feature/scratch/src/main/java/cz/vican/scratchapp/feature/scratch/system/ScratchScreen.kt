0de83da91008977150313d75720960e61d62a47f
Peter Vican <vican.peter@gmail.com>
2025-11-11 02:06:46 +0100

    update ScratchScreen update ScratchViewModel

--- feature/scratch/src/main/java/cz/vican/scratchapp/feature/scratch/system/ScratchScreen.kt
+++ feature/scratch/src/main/java/cz/vican/scratchapp/feature/scratch/system/ScratchScreen.kt
@@ -1,18 +1,27 @@
 package cz.vican.scratchapp.feature.scratch.system
 
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import cz.vican.scratchapp.feature.scratch.presentation.ScratchViewModel
 import org.koin.compose.viewmodel.koinViewModel
 
@@ -35,21 +44,44 @@ private fun ScratchScreenImpl(
     onBackClicked: () -> Unit,
     onScratchCardClicked: () -> Unit,
 ) {
-
     BackHandler(onBack = onBackClicked)
     Scaffold { innerPadding ->
         Column(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(innerPadding)
-                .padding(horizontal = 12.dp)
+                .padding(horizontal = 12.dp),
+            verticalArrangement = Arrangement.Center
         ) {
-            Text(text = state.scratchState)
+            Row(
+                modifier = Modifier
+                    .semantics(
+                        properties = {
+                            contentDescription = "${state.stateTitle}: ${state.scratchState}"
+                        }
+                    )
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    modifier = Modifier.weight(1f),
+                    text = state.stateTitle,
+                    fontSize = 14.sp,
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    modifier = Modifier.weight(1f),
+                    text = state.scratchState,
+                    fontSize = 15.sp,
+                    fontWeight = FontWeight.SemiBold
+                )
+            }
             Button(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(horizontal = 12.dp),
-                onClick = onScratchCardClicked,
+                    .padding(top = 24.dp),
+                onClick = onScratchCardClicked
             ) {
                 Text(text = state.scratchButtonText)
             }
